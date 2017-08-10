cdaa3b72500918be2365708efc3a6475bc61a79f
JeanLive <30533113+JeanLive@users.noreply.github.com>
2017-08-10 17:13:27 -0300

    Add files via upload

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -1193,7 +1193,7 @@ Date dat= new Date();
         
             BLOCO = "" + ((char) 0x1B) + "@" + ((char) 0x1B)
 					+ "j1" + ((char) 0x1B) + "" + ((char) 0x45) + "" + ((char) 0x07)
-					+ ((char) 0x0E) + "" + ((char) 0x14) + ((char) 0x09) + ((char) 0x09) + EMPRESA_DESCRICAO + ((char) 0x1B) + "" + ((char) 0x46)
+					+ ((char) 0x1B)+"a1" + ((char) 0x0E) + "" + ((char) 0x14) + EMPRESA_DESCRICAO + ((char) 0x1B) + "" + ((char) 0x46)
                     + ((char) 0x0A)
                     + ((char) 0x0A)
                     + ((char) 0x13) + "CNPJ"             
@@ -1280,8 +1280,8 @@ Date dat= new Date();
                     + ((char) 0x0A)             
                     /*+ ((char) 0x1B) + "" + ((char) 0x6A) + "0" */+ COMANDA             
                     + ((char) 0x0A) + "" + ((char) 0x0A)             
-                    + ((char) 0x09)
-					/*+ ((char) 0x1B) + "" + ((char) 0x6A) + "2" */+ ((char) 0x1B) + "" + ((char) 0x45) + "wwww.livesistemas.com" + "" + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x0A)             
+                    //+ ((char) 0x09)
+					/*+ ((char) 0x1B) + "" + ((char) 0x6A) + "2" */+ ((char) 0x1B)+"a1"+  ((char) 0x1B) + "" + ((char) 0x45) + "wwww.livesistemas.com" + "" + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x0A)             
                     + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A)
                     + ((char) 0x1B) + "" + ((char) 0x6d);
 			
