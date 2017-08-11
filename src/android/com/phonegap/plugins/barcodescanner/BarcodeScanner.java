e44c646abb4f8942e1abb77d347129c0d20fee17
JeanLive <30533113+JeanLive@users.noreply.github.com>
2017-08-11 15:38:35 -0300

    Add files via upload

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -1057,7 +1057,7 @@ Date dat= new Date();
                     + "                             " + LIVE_PAGAMENTO_DESC
                     + ((char) 0x0A)
                     + "Acrescimo R$:"           
-                    + "                             " + LIVE_PAGAMENTO_ACRES
+                    + "                            " + LIVE_PAGAMENTO_ACRES
                     + ((char) 0x0A)             
                     + ((char) 0x1B) + "" + ((char) 0x45) + "================================================" + ((char) 0x1B) + "" + ((char) 0x46)
                     + ((char) 0x0A)             
@@ -1068,21 +1068,18 @@ Date dat= new Date();
                     + ((char) 0x0A)             
                     + ((char) 0x13) + "TROCO R$:" + "                                " + LIVE_PAGAMENTO_TROCO
                     + ((char) 0x0A) + "" + ((char) 0x0A)             
-                    + "CLIENTE:"
+                    + "CLIENTE"
                     + ((char) 0x3A)
                     + ((char) 0x12) + "" + ((char) 0x0B) + "" + CLIENTE             
                     + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A)             
-                    + ((char) 0x1B) + "" + ((char) 0x45) + "NAO CONTEM VALOR FISCAL!" + ((char) 0x1B) + "" + ((char) 0x46) 
-                    + ((char) 0x0A) + "" + ((char) 0x0A)             
-                    + ((char) 0x0E) + "" + ((char) 0x14) + MENSAGEM            
+                    + ((char) 0x1B)+"a1" + "NAO CONTEM VALOR FISCAL!" 
+                    + ((char) 0x0A) + ((char) 0x0A)            
+                    + ((char) 0x1B) +"a1" + MENSAGEM     
                     + ((char) 0x0A) + "" + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "0" + "OPERADOR"
-                    + ((char) 0x3A)             
-                    + ((char) 0x12) + "" + ((char) 0x0B) + "" + USUARIO
-                    + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "0" + COMANDA
+                    + ((char) 0x1B)+"a0" + "OPERADOR:" + "" + USUARIO
+                    + ((char) 0x0A)             
+                    + COMANDA
                     + ((char) 0x0A) + "" + ((char) 0x0A)                          
-                    + ((char) 0x09)
 					+ ((char) 0x1B)+"a1"+  ((char) 0x1B) + ((char) 0x45) + "wwww.livesistemas.com" + ((char) 0x1B) + ((char) 0x46) + ((char) 0x0A)
                     + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A) + "" + ((char) 0x0A)
                     + ((char) 0x1B) + "" + ((char) 0x6d));             
