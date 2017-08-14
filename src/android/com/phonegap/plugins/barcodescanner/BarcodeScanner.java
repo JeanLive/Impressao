b30dd85504bc49d6093a72078717efaa230b3595
JeanLive <30533113+JeanLive@users.noreply.github.com>
2017-08-14 12:00:10 -0300

    Add files via upload

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -1352,26 +1352,26 @@ Date dat= new Date();
                     + "                          " + LIVE_TOTAL_ITENS
                     + ((char) 0x0A)             
                     + ACRES_DESC             
-                    + ((char) 0x1B) + "" + ((char) 0x45) + "Valor a Pagar R$                         " + VLR_RECEBER + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  
+                    + ((char) 0x1B)+"a0" + ((char) 0x1B) + "" + ((char) 0x45) + "Valor a Pagar R$                         " + VLR_RECEBER + ((char) 0x1B) + "" + ((char) 0x46)  
                     + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "0" + "FORMA DE PAGAMENTO                 VALOR PAGO R$"
+                    + ((char) 0x1B)+"a0" + "FORMA DE PAGAMENTO                 VALOR PAGO R$"
                     + ((char) 0x0A)             
                     + ((char) 0x13) + VENDA_PAGAMENTO
                     + ((char) 0x0A)
                     + ((char) 0x13) + "TROCO R$" + "                                 " + LIVE_PAGAMENTO_TROCO   
                     + ((char) 0x0A) + ((char) 0x0A)             
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "1" + ((char) 0x1B) + "" + ((char) 0x45) + "Consulte pela Chave de Acesso em" + ((char) 0x1B) + "" + ((char) 0x46)
+                    + ((char) 0x1B)+"a1" + ((char) 0x1B) + ((char) 0x45) + "Consulte pela Chave de Acesso em" + ((char) 0x1B) + "" + ((char) 0x46)
                     + ((char) 0x0A)             
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "1" + URL_CONSULTA
+                    + ((char) 0x1B)+"a1" + URL_CONSULTA
                     + ((char) 0x0A)             
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "1" + CHAVE_ACESSO          
-                    + ((char) 0x0A) + "" + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x45) + CONSUMIDOR + "" + ((char) 0x1B) + "" + ((char) 0x46) + CLIENTE + ((char) 0x1B) + "" + ((char) 0x6A) + "1"                                     
-                    + ((char) 0x0A) + "" + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x45) + "NFC-e nº " + NUM_CUPOM + ((char) 0x20) + "Serie " + SERIE + ((char) 0x20) + LIVE_DATA + ((char) 0x20) + LIVE_HORA + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x45) + "Protocolo de autorização:" + ((char) 0x1B) + "" + ((char) 0x46) +  PROTOCOLO + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x45) + "Data da autorização" + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x20) + DATA_HORA_AUTORIZACAO + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  + ((char) 0x0A) + ((char) 0x0A)
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "1" + "EMITIDA EM AMBIENTE DE HOMOLOGAÇÃO - SEM VALOR   FISCAL"
+                    + ((char) 0x1B)+"a1" + CHAVE_ACESSO          
+                    + ((char) 0x0A) + ((char) 0x0A)
+                    + ((char) 0x1B)+"a1" + ((char) 0x1B) + "" + ((char) 0x45) + CONSUMIDOR + "" + ((char) 0x1B) + "" + ((char) 0x46) + CLIENTE
+                    + ((char) 0x0A) + ((char) 0x0A)
+                    + ((char) 0x1B) + ((char) 0x45) + "NFC-e nº " + NUM_CUPOM + ((char) 0x20) + "Serie " + SERIE + ((char) 0x20) + LIVE_DATA + ((char) 0x20) + LIVE_HORA + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  + ((char) 0x0A)
+                    + ((char) 0x1B) + "" + ((char) 0x45) + "Protocolo de autorizacao:" + ((char) 0x1B) + "" + ((char) 0x46) +  PROTOCOLO + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  + ((char) 0x0A)
+                    + ((char) 0x1B) + "" + ((char) 0x45) + "Data da autorizacao" + ((char) 0x1B) + "" + ((char) 0x46) + ((char) 0x20) + DATA_HORA_AUTORIZACAO + ((char) 0x1B) + "" + ((char) 0x6A) + "1"  + ((char) 0x0A) + ((char) 0x0A)
+                    + ((char) 0x1B)+"a1" + "EMITIDA EM AMBIENTE DE HOMOLOGAÇÃO - SEM VALOR   FISCAL"
                     + ((char) 0x0A) + ((char) 0x0A)             
                     + montarQrCode(LIVE_QRCODE, "H", "4")
                     + ((char) 0x0A)             
