f10c3638b0622cc68cb92762e124f8ae11c0c948
JeanLive <30533113+JeanLive@users.noreply.github.com>
2017-08-18 12:05:13 -0300

    Add files via upload

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -57,6 +57,7 @@ public class BarcodeScanner extends CordovaPlugin {
     String LIVE_PAGAMENTO_TROCO;
     String ITENS;
     String VENDA_ITENS;
+    String VENDA_ITENS_REDE;
     String VENDA_ITENS_BLUETOOTH;
     String VENDA_PAGAMENTO;
     String TIPO;
@@ -372,6 +373,30 @@ public void live(JSONArray args){
                     COMANDA="";    
                     }
                     
+                    if(IMPRESSAO_TIPO.equals("Rede")){
+                    VENDA_ITENS_REDE=((char) 0x1B) + "" + ((char) 0x6A) + "0" + LIVE_ITENS_PRODUTOID 
+                    + "" +((char) 0x20) +((char) 0x20) 
+                    + "" + LIVE_ITENS_DESCRICAO 
+                    + ((char) 0x0A) 
+                    + ((char) 0x1B) + "" + ((char) 0x6A) + "2" 
+                    + "" + LIVE_ITENS_QUANTIDADE 
+                    + "" + ((char) 0x20) + "" + ((char) 0x20) 
+                    + "" + LIVE_ITENS_UNIDADE + ((char) 0x20) + ((char) 0x20) 
+                    + "" + LIVE_ITENS_VLR_UNIT + ((char) 0x20) + ((char) 0x20) + "   " + LIVE_ITENS_VLR_TOTAL + ((char) 0x0A); 
+                        
+                    }else{
+                        VENDA_ITENS_REDE=((char) 0x1B)+"a0"+ LIVE_ITENS_PRODUTOID 
+                    +((char) 0x20) +((char) 0x20) 
+                    + LIVE_ITENS_DESCRICAO 
+                    + ((char) 0x0A)
+                    + ((char) 0x1B)+"a2" 
+                    + "" + LIVE_ITENS_QUANTIDADE 
+                    + "" + ((char) 0x20) + "" + ((char) 0x20) 
+                    + "" + LIVE_ITENS_UNIDADE + ((char) 0x20) + ((char) 0x20) 
+                    + "" + LIVE_ITENS_VLR_UNIT + ((char) 0x20) + ((char) 0x20) + "   " + LIVE_ITENS_VLR_TOTAL 
+                    + ((char) 0x0A);
+                    }
+                    
                     itens = object.getJSONObject(i).getJSONArray("itens");
                     moeda = object.getJSONObject(i).getJSONArray("pagamentos");
                     int SEQ=1;
@@ -394,15 +419,7 @@ public void live(JSONArray args){
                     LIVE_OBSERVACAO = itens.getJSONObject(y).getString("live_observacao").toString();    
                        
                     //+ ((char) 0x1B) + "" + ((char) 0x6A) + "0" + String.format("%03d", SEQ)    
-                    VENDA_ITENS+=((char) 0x1B) + "" + ((char) 0x6A) + "0" + LIVE_ITENS_PRODUTOID 
-                    + "" +((char) 0x20) +((char) 0x20) 
-                    + "" + LIVE_ITENS_DESCRICAO 
-                    + ((char) 0x0A) 
-                    + ((char) 0x1B) + "" + ((char) 0x6A) + "2" 
-                    + "" + LIVE_ITENS_QUANTIDADE 
-                    + "" + ((char) 0x20) + "" + ((char) 0x20) 
-                    + "" + LIVE_ITENS_UNIDADE + ((char) 0x20) + ((char) 0x20) 
-                    + "" + LIVE_ITENS_VLR_UNIT + ((char) 0x20) + ((char) 0x20) + "   " + LIVE_ITENS_VLR_TOTAL + ((char) 0x0A);
+                    VENDA_ITENS+=VENDA_ITENS_REDE;
                     SEQ++;
                     };
                    
