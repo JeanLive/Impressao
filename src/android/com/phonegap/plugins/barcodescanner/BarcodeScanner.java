f5a48e2687a0452fbaedee30b82254f50e1485b3
JeanLive <30533113+JeanLive@users.noreply.github.com>
2017-08-18 11:31:38 -0300

    Add files via upload

--- src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
+++ src/android/com/phonegap/plugins/barcodescanner/BarcodeScanner.java
@@ -419,7 +419,7 @@ public void live(JSONArray args){
                     VLR_RECEBER = moeda.getJSONObject(x).getString("vlr_apagar").toString();    
                     LIVE_PAGAMENTO_TROCO = moeda.getJSONObject(x).getString("pag_troco").toString();
                       
-                   VENDA_PAGAMENTO+= ((char) 0x1B)+"a0"+ LIVE_PAGAMENTO_MOEDA +"                         "+ LIVE_PAGAMENTO_RECEBIDO
+                   VENDA_PAGAMENTO+= ((char) 0x1B) + "" + ((char) 0x6A) + "0" + LIVE_PAGAMENTO_MOEDA +"                         "+ LIVE_PAGAMENTO_RECEBIDO
                     + ((char) 0x0A);
                                     
                     };
